e81831a200fe238b998a70239b88b00c79e22bcf
Vivek Patil <vivekpatil1718@gmail.com>
2024-02-06 23:33:43 +0530

    paddle move logic

--- Ping Pong game/index.js
+++ Ping Pong game/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let table = document.getElementById('ping-pong-table')
     let ball = document.getElementById('ball'); 
+    let paddle = document.getElementById('paddle');
+
     //ballX and ballY starting point of table w.r.t table
     let ballX = 50; // distance of the left of the ball w.r.t. ping pong table
     let ballY = 50; // distance of the top of the ball w.r.t. ping pong table
@@ -19,8 +21,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
         //  if(ballX > 500-20 || ballX  <= 0)  dx *= -1;
         //  if(ballY > 300-20 || ballY <= 0) dy *= -1;
 
-        if(ballX > table.offsetWidth-ball.offsetWidth || ballX <= 0){ dx *= -1};
-        if(ballY > table.offsetHeight-ball.offsetHeight || ballY <= 0){ dy *= -1};
+        if(ballX > table.offsetWidth-ball.offsetWidth || ballX <= 0){ dx *= -1}; // change x-direction
+        if(ballY > table.offsetHeight-ball.offsetHeight || ballY <= 0){ dy *= -1}; // change y-dorection
 
     },1)
+    
+    let paddleY = 0;
+    let dPy = 5;
+    document.addEventListener('keydown', (event)=>{
+        console.log(table.offsetHeight,paddleY)
+       if(event.keyCode == 38 && paddleY > 0){
+        //up arrow
+        paddleY += (-1)*dPy;
+       }else if(event.keyCode == 40 && paddleY < table.offsetHeight-paddle.offsetHeight){
+        //down arrow
+        paddleY += dPy;
+       } 
+       paddle.style.top = `${paddleY}px`;
+    })
 })
